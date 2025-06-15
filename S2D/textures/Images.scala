7aa3c56e721387c3df314e80edf33f51f061fa48
Matias <matias.finochio@davinci.edu.ar>
2025-06-15 11:58:19 -0300

    added loadRaw function to Images.scala

--- S2D/textures/Images.scala
+++ S2D/textures/Images.scala
@@ -6,7 +6,7 @@ import sdl2.Extras.*
 import scalanative.unsafe.*
 import scalanative.unsigned.*
 import scala.util.Using
-import scalanative.libc.{stdlib, string}
+import scalanative.libc.{stdlib, string, stdio}
 
 object Images:
   def load(fileName: String): Option[Image] =
@@ -48,4 +48,47 @@ object Images:
             SDL_FreeSurface(surface)
       catch
         case _: Exception => None
+    }
+
+  def loadRaw(fileName: String, width: Int, height: Int, format: Int, headerSize: Int): Option[Image] =
+    Zone {
+        try
+          val fileNameCStr = toCString(fileName)
+          val file = stdio.fopen(fileNameCStr, c"rb")
+
+          if file == null then
+            return None
+
+          try
+            stdio.fseek(file, 0, stdio.SEEK_END)
+            val fileSize = stdio.ftell(file)
+            stdio.fseek(file, 0, stdio.SEEK_SET)
+
+            if fileSize <= headerSize then
+              return None
+
+            val pixelFormat = PixelFormat.fromValue(format).getOrElse(PixelFormat.UncompressedR8G8B8A8)
+            val expectedDataSize = width * height * pixelFormat.bytesPerPixel
+            val availableDataSize = fileSize - headerSize
+
+            if availableDataSize < expectedDataSize then
+              return None
+
+            if headerSize > 0 then
+              stdio.fseek(file, headerSize, stdio.SEEK_SET)
+
+            val imageData = stdlib.malloc(expectedDataSize.toUSize).asInstanceOf[Ptr[Byte]]
+            if imageData == null then
+              return None
+
+            val bytesRead = stdio.fread(imageData, 1.toUSize, expectedDataSize.toUSize, file)
+            if bytesRead != expectedDataSize.toUSize then
+              stdlib.free(imageData)
+              return None
+
+            Some(Image(imageData, width, height, 1, format))
+          finally
+            stdio.fclose(file)
+        catch
+          case _: Exception => None
     }
