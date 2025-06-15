b5b7f9114cf5c9d762adea7a6977d1d09a016b8b
Matias <matias.finochio@davinci.edu.ar>
2025-06-14 21:43:06 -0300

    finished functions for Cursor.scala and finished core package

--- S2D/core/Cursor.scala
+++ S2D/core/Cursor.scala
@@ -24,3 +24,33 @@ object Cursor:
     if !Window.isWindowInitialized then return false
 
     SDL_ShowCursor(-1) == 0
+
+  def enable(): Unit =
+    if !Window.isWindowInitialized then return
+
+    SDL_ShowCursor(1)
+
+  def disable(): Unit =
+    if !Window.isWindowInitialized then return
+
+    SDL_SetRelativeMouseMode(1.toUInt)
+
+  def isOnScreen: Boolean =
+    if !Window.isWindowInitialized then return false
+
+    Zone {
+      val xPos = stackalloc[CInt]()
+      val yPos = stackalloc[CInt]()
+      SDL_GetMouseState(xPos, yPos)
+
+      val width = stackalloc[CInt]()
+      val height = stackalloc[CInt]()
+      SDL_GetWindowSize(Window.windowHandle, width, height)
+
+      val x = !xPos
+      val y = !yPos
+      val w = !width
+      val h = !height
+
+      x >= 0 && x < w && y >= 0 && y < h
+    }
