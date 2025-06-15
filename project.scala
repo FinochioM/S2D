7f7d09cd6e6c1e39639f4307c26dcd4fd33cc3aa
Matias <matias.finochio@davinci.edu.ar>
2025-06-15 17:49:55 -0300

    0.1.3 - updated project.scala file

--- project.scala
+++ project.scala
@@ -2,3 +2,14 @@
 //> using platform scala-native
 //> using nativeVersion 0.5.8
 //> using scalacOptions -Wconf:msg=indented:silent
+
+//> using publish.computeVersion git:tag
+//> using publish.developers "FinochioM|Matias Finochio|https://github.com/FinochioM"
+//> using publish.license zlib
+//> using publish.name S2D
+//> using publish.organization io.github.finochiom
+//> using publish.repository central
+//> using publish.secretKey env:PGP_SECRET
+//> using publish.secretKeyPassword env:PGP_PASSPHRASE
+//> using publish.url https://github.com/FinochioM/S2D
+//> using publish.vcs github:finochiom/S2D
