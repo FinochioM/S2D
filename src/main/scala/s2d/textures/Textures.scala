72dffb195cac941d305727faefcb733c694f01cc
Matias <matias.finochio@davinci.edu.ar>
2025-06-16 16:46:55 -0300

    0.1.6-SNAPSHOT - added more functions to Textures.scala

--- src/main/scala/s2d/textures/Textures.scala
+++ src/main/scala/s2d/textures/Textures.scala
@@ -165,3 +165,117 @@ object Textures:
       }
     catch
       case _: Exception => None
+
+  def loadRenderTexture(width: Int, height: Int): Option[RenderTexture2D] =
+    try
+      if width <= 0 || height <= 0 then return None
+
+      Zone {
+        val framebufferIdArray = alloc[GLuint](1)
+        glGenFramebuffers(1.toUInt, framebufferIdArray)
+        val framebufferId = !framebufferIdArray
+        if framebufferId == 0.toUInt then return None
+
+        val colorTextureIdArray = alloc[GLuint](1)
+        glGenTextures(1.toUInt, colorTextureIdArray)
+        val colorTextureId = !colorTextureIdArray
+        if colorTextureId == 0.toUInt then
+          val framebufferDeleteArray = alloc[GLuint](1)
+          !framebufferDeleteArray = framebufferId
+          glDeleteFramebuffers(1.toUInt, framebufferDeleteArray)
+          return None
+
+        val depthRenderbufferIdArray = alloc[GLuint](1)
+        glGenRenderbuffers(1.toUInt, depthRenderbufferIdArray)
+        val depthRenderbufferId = !depthRenderbufferIdArray
+        if depthRenderbufferId == 0.toUInt then
+          val framebufferDeleteArray = alloc[GLuint](1)
+          !framebufferDeleteArray = framebufferId
+          glDeleteFramebuffers(1.toUInt, framebufferDeleteArray)
+          val textureDeleteArray = alloc[GLuint](1)
+          !textureDeleteArray = colorTextureId
+          glDeleteTextures(1.toUInt, textureDeleteArray)
+          return None
+
+        glBindTexture(GL_TEXTURE_2D.toUInt, colorTextureId)
+        glTexImage2D(GL_TEXTURE_2D.toUInt, 0, GL_RGBA.toInt, width.toUInt, height.toUInt, 0, GL_RGBA.toUInt, GL_UNSIGNED_BYTE.toUInt, null.asInstanceOf[Ptr[Byte]])
+        glTexParameteri(GL_TEXTURE_2D.toUInt, GL_TEXTURE_MIN_FILTER.toUInt, GL_LINEAR.toInt)
+        glTexParameteri(GL_TEXTURE_2D.toUInt, GL_TEXTURE_MAG_FILTER.toUInt, GL_LINEAR.toInt)
+        glTexParameteri(GL_TEXTURE_2D.toUInt, GL_TEXTURE_WRAP_S.toUInt, GL_CLAMP_TO_EDGE.toInt)
+        glTexParameteri(GL_TEXTURE_2D.toUInt, GL_TEXTURE_WRAP_T.toUInt, GL_CLAMP_TO_EDGE.toInt)
+        glBindTexture(GL_TEXTURE_2D.toUInt, 0.toUInt)
+
+        glBindRenderbuffer(GL_RENDERBUFFER.toUInt, depthRenderbufferId)
+        glRenderbufferStorage(GL_RENDERBUFFER.toUInt, GL_DEPTH_COMPONENT.toUInt, width.toUInt, height.toUInt)
+        glBindRenderbuffer(GL_RENDERBUFFER.toUInt, 0.toUInt)
+
+        glBindFramebuffer(GL_FRAMEBUFFER.toUInt, framebufferId)
+        glFramebufferTexture2D(GL_FRAMEBUFFER.toUInt, GL_COLOR_ATTACHMENT0.toUInt, GL_TEXTURE_2D.toUInt, colorTextureId, 0)
+        glFramebufferRenderbuffer(GL_FRAMEBUFFER.toUInt, GL_DEPTH_ATTACHMENT.toUInt, GL_RENDERBUFFER.toUInt, depthRenderbufferId)
+
+        val status = glCheckFramebufferStatus(GL_FRAMEBUFFER.toUInt)
+        if status != GL_FRAMEBUFFER_COMPLETE.toUInt then
+          val framebufferDeleteArray = alloc[GLuint](1)
+          !framebufferDeleteArray = framebufferId
+          glDeleteFramebuffers(1.toUInt, framebufferDeleteArray)
+          val textureDeleteArray = alloc[GLuint](1)
+          !textureDeleteArray = colorTextureId
+          glDeleteTextures(1.toUInt, textureDeleteArray)
+          val renderbufferDeleteArray = alloc[GLuint](1)
+          !renderbufferDeleteArray = depthRenderbufferId
+          glDeleteRenderbuffers(1.toUInt, renderbufferDeleteArray)
+          glBindFramebuffer(GL_FRAMEBUFFER.toUInt, 0.toUInt)
+          return None
+
+        glBindFramebuffer(GL_FRAMEBUFFER.toUInt, 0.toUInt)
+
+        val colorTexture = Texture2D(colorTextureId.toInt, width, height, PixelFormat.UncompressedR8G8B8A8.value, 1)
+        Some(RenderTexture2D(framebufferId.toInt, colorTexture, depthRenderbufferId.toInt))
+      }
+    catch
+      case _: Exception => None
+
+  def isValid(texture: Texture2D): Boolean =
+    try
+      texture.id > 0 && glIsTexture(texture.id.toUInt) != 0.toUByte
+    catch
+      case _: Exception => false
+
+  def unload(texture: Texture2D): Unit =
+    try
+      if texture.id > 0 then
+        Zone {
+          val textureArray = alloc[GLuint](1)
+          !textureArray = texture.id.toUInt
+          glDeleteTextures(1.toUInt, textureArray)
+        }
+    catch
+      case _: Exception =>
+
+  def isRenderTextureValid(target: RenderTexture2D): Boolean =
+    try
+      target.id > 0 &&
+        glIsFramebuffer(target.id.toUInt) != 0.toUByte &&
+        isValid(target.texture) &&
+        target.depth > 0 &&
+        glIsRenderbuffer(target.depth.toUInt) != 0.toUByte
+    catch
+      case _: Exception => false
+
+  def unloadRenderTexture(target: RenderTexture2D): Unit =
+    try
+      Zone {
+        if target.id > 0 then
+          val framebufferArray = alloc[GLuint](1)
+          !framebufferArray = target.id.toUInt
+          glDeleteFramebuffers(1.toUInt, framebufferArray)
+
+        unload(target.texture)
+
+        if target.depth > 0 then
+          val renderbufferArray = alloc[GLuint](1)
+          !renderbufferArray = target.depth.toUInt
+          glDeleteRenderbuffers(1.toUInt, renderbufferArray)
+      }
+    catch
+      case _: Exception =>
